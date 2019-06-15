9a2ba71b625209e80cece58925739f4a8ae7bfcc
Regina Mc Guire <rmcguire@antelecom.net>
2019-06-15 15:00:18 -0700

    Display test meal titles

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import meals from "../../tests/meals"
 
 const Results = (props) => (
             <div>
                 <h1>Results for {props.countryName} Menus</h1>
+                {meals.map((meal) => 
+                    <h1 key={meal.idMeal}>{meal.menuTitle}</h1>)}
             </div>   
 )
 
