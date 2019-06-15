f006d2e8d1e33bb902b7ee00218f9ddad1ae81f4
Regina Mc Guire <rmcguire@antelecom.net>
2019-06-15 14:57:47 -0700

    Add Results component

---
+++ src/components/Results/Results.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Results = (props) => (
+            <div>
+                <h1>Results for {props.countryName} Menus</h1>
+            </div>   
+)
+
+export default Results;
