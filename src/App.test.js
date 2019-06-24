5870325bf7f4df98d2f6a80e81adf2727ef70c1f
Brian <trangb@oregonstate.edu>
2019-06-24 01:39:34 -0700

    downgrade jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter:new EnzymeAdapter()});
+
+test('renders without Error',()=>{
+
 });
