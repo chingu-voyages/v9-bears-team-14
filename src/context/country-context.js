77b155b202d6078308a35e69bc81cf9643ba821b
Brian <trangb@oregonstate.edu>
2019-06-24 18:07:21 -0700

    removed unused imports

--- src/context/country-context.js
+++ src/context/country-context.js
@@ -1,4 +1,4 @@
-import React,{useContext,useState} from 'react'
+import React,{useContext} from 'react'
 
 //const CountryContext = React.createContext();                     //not initializing createContext means tests fail
 const CountryContext = React.createContext({countrySelected:""});//create custom hook to retrieve context for testing, otherwise test fails for Map component //https://medium.com/7shifts-engineering-blog/testing-usecontext-react-hook-with-enzyme-shallow-da062140fc83
