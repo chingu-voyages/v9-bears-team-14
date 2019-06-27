48d096ed84a39ffeaa414890feb2f683862684fb
Brian <trangb@oregonstate.edu>
2019-06-26 18:18:20 -0700

    moving directory since backend package.json needs to be at root

---
+++ frontend/build/precache-manifest.f27f59a0a310f9f4c1c38a22d99c9f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aeedcd927e6dc9635860a37a8480fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60dba3d9e44fd00607e6",
+    "url": "/static/css/main.5fdd0ec8.chunk.css"
+  },
+  {
+    "revision": "4c533f73515cf7e6173b",
+    "url": "/static/js/2.4e343048.chunk.js"
+  },
+  {
+    "revision": "60dba3d9e44fd00607e6",
+    "url": "/static/js/main.ea0922af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
