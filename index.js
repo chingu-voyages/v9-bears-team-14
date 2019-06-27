a54cde24e5e22ac5c19e313b71dbc2323b9ba788
Brian <trangb@oregonstate.edu>
2019-06-26 18:46:14 -0700

    removed /

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if(process.env.NODE_ENV === 'production'){
     //express will serve index.html if it doesn't recognize the route
     const path =require('path');
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend/','build','index.html'));
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
     })
 }
 
