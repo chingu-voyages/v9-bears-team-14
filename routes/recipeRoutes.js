2a4cd7e06f1f4dc5ad9301757b1499f2fad7b17e
Brian <trangb@oregonstate.edu>
2019-06-26 19:12:33 -0700

    deleting default route

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,9 +1,6 @@
 const axios = require('axios');     //http client
 module.exports=app=>{
-    app.get('/',(req,res)=>{
-        res.send({hi:'there from Geo-foods'})
-    })
-    
+
     /**
      * @function - this is a route that handles GET requests for a country's recipes, takes in a country name params
      * @return {array} - sends an array of recipe objects {strMeal: "Beef lo Mein", strMealThumb: "https://www.themealdb.com/images/media/meals/1529444830.jpg","idMeal":"52952"}
