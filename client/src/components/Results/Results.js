5c17c4e554b8da0d80c2355135771dc566e38053
Brian <trangb@oregonstate.edu>
2019-07-01 17:53:15 -0700

    fixing conflicts regarding stylesheet

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import React, { useContext } from "react";
 import useRecipes from "../../hooks/useRecipes";
 import CountryContext from "../../context/country-context";
 import MAP_CONSTANTS from "../Map/MapConstants/MAP_CONSTANTS";
 import LoadSpinner from '../LoadingModal/LoadSpinner';
-import "./Results.css";
+import "./_results.scss";
 
 
 const Results = () => {
@@ -13,8 +12,8 @@ const Results = () => {
   const renderRecipes = ()=>{
         
     return(    <React.Fragment>
-                    <h1 className="Results__Title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>         
-                    <div className="Results__Wrapper">
+                    <h1 className="Results--title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>         
+                    <div className="Results--wrapper">
                         {recipes.map((meal) => 
                             <div key={meal.idMeal} className="Results__Card">
                                 <h1  >{meal.strMeal}</h1>
@@ -25,35 +24,12 @@ const Results = () => {
                 </React.Fragment>
             );
 }
-=======
-import React, {useContext} from 'react'
-import useRecipes from '../../hooks/useRecipes';
-import CountryContext from '../../context/country-context'
-import MAP_CONSTANTS from '../Map/MapConstants/MAP_CONSTANTS'
->>>>>>> development
 
 return (
-<<<<<<< HEAD
-            <div className="Results__Container">
+            <div className="Results--wrapper">
                 {isError && <div>Something went wrong ...</div>}
                 {isLoading? <LoadSpinner/> : recipes.length?renderRecipes():null}
             </div>
-=======
-    <div >
-        <div className="Results--wrapper">
-        
-        {countrySelected.length > 0 &&<h1 className="Results--title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}         
-        </div>
-        <div className="Results--wrapper">
-            {recipes.map((meal) => 
-                <div key={meal.idMeal} className="Results--card ">
-                    <h1  >{meal.strMeal}</h1>
-                    <img className="Results--image"src={meal.strMealThumb} alt={meal.strMeal}/>
-                </div>
-            )} 
-        </div> 
-    </div>
->>>>>>> development
     )
 };
 export default Results;
