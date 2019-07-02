8062a00b4527a1355192df31adb5cefc99197b72
Brian <trangb@oregonstate.edu>
2019-07-02 10:55:46 -0700

    converting all margins and padding to locks and vmin

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -7,15 +7,14 @@ const Results = (props) => {
 const {countrySelected} = useContext(CountryContext)
 const recipes = useRecipes(countrySelected)
 return (
-    <div >
+    <div className="Results--container">
         <div className="Results--wrapper">
-        
-        {countrySelected.length > 0 &&<h1 className="Results--title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}         
+            {countrySelected.length > 0 &&<h1 className="Results--title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}         
         </div>
         <div className="Results--wrapper">
             {recipes.map((meal) => 
-                <div key={meal.idMeal} className="Results--card ">
-                    <h1  >{meal.strMeal}</h1>
+                <div key={meal.idMeal} className="Results--card">
+                    <h1  className="Results--card--title">{meal.strMeal}</h1>
                     <img className="Results--image"src={meal.strMealThumb} alt={meal.strMeal}/>
                 </div>
             )} 
