d63efc7ec12141946531461896fda0339a8f9bab
Regina Mc Guire <rmcguire@antelecom.net>
2019-07-10 22:41:25 -0700

    Add database to server

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID:'921560253017-m04bivou4athcso6fklatb01s4ip5a5r.apps.googleusercontent.com',
+    googleClientSecret: '5WzpeUYwxY87wVK0l05JJcRf',
+    mongoURI: 'mongodb+srv://geo-user:hT7rMmv9YL6zGAiT@cluster0-gvkw8.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'Lunathecatonthebookshelf'
+}
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+const keys = require('./config/keys')
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
+
 const app = express();
+// to support JSON-encoded bodies
+app.use(express.json());  
 
-app.use(express.json());       // to support JSON-encoded bodies
-
-require('./routes/recipeRoutes')(app);
-
-if(process.env.NODE_ENV === 'production'){
-
-    //express will serve up our production assets
-    //like main.js file
-    app.use(express.static('client/build'));        
-    //express will serve index.html if it doesn't recognize the route
-    const path =require('path');
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
+app.get('/users', async(req, res) => {
+    try {
+        const users = await User.find({ })
+        res.send(users)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 const PORT = process.env.PORT|| 5000;
-app.listen(PORT); 
-
+app.listen(PORT) 
