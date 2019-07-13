58ef96bf7dd88b37735ff7625e4bd9a03c4c1d74
Brian <trangb@oregonstate.edu>
2019-07-12 20:01:35 -0700

    added hover color for title

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import React from 'react';
-const Header =(props)=>{
-    return(
-        <header className="Header--wrapper">
-            <h1 className="Header--title">Geo Foods</h1>
-            <h2 className="Header--title Header--subtitle">Explore the World's Cuisines</h2>
-        </header>
-    )
-}
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = props => {
+  return (
+    <header className="Header--wrapper">
+      <Link to="/">
+        <h1 className="Header--title">Geo Foods</h1>
+      </Link>
+      <h2 className=" Header--subtitle">
+        Explore the World's Cuisines
+      </h2>
+    </header>
+  );
+};
 
 export default Header;
