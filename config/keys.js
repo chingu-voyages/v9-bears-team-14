a78a15288057836990ada5b0ada68009be199571
Regina Mc Guire <rmcguire@antelecom.net>
2019-07-13 10:49:35 -0700

    Add authorization code for user authentication

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID:'921560253017-m04bivou4athcso6fklatb01s4ip5a5r.apps.googleusercontent.com',
-    googleClientSecret: '5WzpeUYwxY87wVK0l05JJcRf',
-    mongoURI: 'mongodb+srv://geo-user:hT7rMmv9YL6zGAiT@cluster0-gvkw8.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'Lunathecatonthebookshelf'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod')
+} else {
+    module.exports = require('./dev')
 }
