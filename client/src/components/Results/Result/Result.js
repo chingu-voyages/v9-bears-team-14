da9d4bc2fdb1a9b457e3912dfffd76fc18b1621d
Brian <trangb@oregonstate.edu>
2019-07-27 18:52:37 -0700

    trying to fix animation

--- client/src/components/Results/Result/Result.js
+++ client/src/components/Results/Result/Result.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Result.css";
 
-const Result = ({meal}) => {
+const Result = ({meal,style}) => {
 
     const limitRecipeTitle = (title, limit = 25) => {
         const newTitle = [];
@@ -34,6 +34,25 @@ const Result = ({meal}) => {
           </h1>
         </div>
       </div >
+    //   <animated.div
+    //   key={meal.idMeal}
+    //   className="Results--card"
+    //   style={style}
+    // >
+    //   <img
+    //     className="Results--image"
+    //     src={meal.strMealThumb}
+    //     alt={meal.strMeal}
+    //   />
+    //   <div className="after">
+    //     <h1 className="Results--card--title">
+    //       {/* {meal.strMeal} */}
+    //       <Link to={`/recipe/${meal.idMeal}`} key={meal.idMeal}>
+    //         {limitRecipeTitle(meal.strMeal)}
+    //       </Link>
+    //     </h1>
+    //   </div>
+    // </animated.div>
     
   );
 };
--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -1,40 +1,47 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import useResults from "../../hooks/useResults";
-// import { useTrail, animated,useTransition } from 'react-spring'
 import CountryContext from "../../context/country-context";
-import Result from './Result/Result';
-import LoadSpinner from '../LoadingSpinner/LoadSpinner';
+//import Result from "./Result/Result";
+import LoadSpinner from "../LoadingSpinner/LoadSpinner";
 import MAP_CONSTANTS from "../Map/MapConstants/MAP_CONSTANTS";
+import ResultCards from './ResultCards';
+import "./Result/Result.css";
 import "./_results.scss";
 
 const Results = () => {
-    const {countrySelected} = useContext(CountryContext)
-    const [{recipes,isLoading,isError}] = useResults(countrySelected);
+  const { countrySelected } = useContext(CountryContext);
+  const [{ recipes, isLoading, isError }] = useResults(countrySelected);
 
 
-    const renderRecipes = ()=>{
+  const renderRecipes = () => {
+      
+    return (
+      <React.Fragment>
+        <div className="Results--Header--title">
+          {countrySelected.length > 0 && (
+            <h1 className="Results--title">
+              {MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes
+            </h1>
+          )}
+        </div>
+        <div className="Results--wrapper">
+          <ResultCards recipes={recipes}/>
+        </div>
+      </React.Fragment>
+    );
+  };
 
-
-        return(    <React.Fragment>
-                        <div className="Results--Header--title">
-                            {countrySelected.length > 0 &&<h1 className="Results--title">{MAP_CONSTANTS.supportedCountries[countrySelected]} Recipes</h1>}      
-                        </div>  
-                        <div className="Results--wrapper">
-                            {recipes.map((meal) => <Result key={meal.idMeal} meal={meal}/>)} 
-                        </div>
-                    </React.Fragment>
-                );
-    }
-
-
-
-
-return (
+  return (
     <div className="Results--container">
-        {isError && <div className="Results--error">Something went wrong ...</div>}
-        {isLoading? <LoadSpinner color={"black"} message={"Loading your results"}/> : recipes.length?renderRecipes():null}
+      {isError && (
+        <div className="Results--error">Something went wrong ...</div>
+      )}
+      {isLoading ? (
+        <LoadSpinner color={"black"} message={"Loading your results"} />
+      ) : recipes.length ? (
+        renderRecipes()
+      ) : null}
     </div>
-    )
-}
+  );
+};
 export default Results;
-
